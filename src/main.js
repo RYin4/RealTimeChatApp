19c7471926dd45791aa24231a9a365b7afe7706e
Randy Yin <randyyin28@gmail.com>
2019-03-16 15:41:58 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import vueChatScroll from 'vue-chat-scroll';
 import App from "./App.vue";
 import router from "./router";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
+Vue.use(vueChatScroll);
 
 new Vue({
   router,
