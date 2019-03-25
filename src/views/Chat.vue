bf5f51d28b911e761aa08bc0b1a4b6fb8d47e262
Randy Yin <randyyin28@gmail.com>
2019-03-25 16:59:26 -0700

    Update Chat.vue

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="container chat">
-        <h2 class="text-primary text-center">Real Time Chat</h2>
-        <h5 class="text-secondary text-center">Powered by Vue.js and Firebase</h5>
+        <h2 class="text-primary text-center">Real-Time Chat</h2>
+        <h5 class="text-secondary text-center">Powered by Vue.js & Firebase</h5>
         <div class="card">
             <div class="card-body">
-                <p class="text-secondary nomessages" v-if="messages.length == 0">[No Messages Yet!]</p>
-                <div class="messages" v-chat-scroll="{always: false, smooth: true}">  
-                    <div v-for="message in messages" :key="message.id"> 
-                        <span class="text-info">[{{message.name}}]:  </span>
-                        <span> {{message.message}}</span>
+                <p class="text-secondary nomessages" v-if="messages.length == 0">
+                    [No messages yet!]
+                </p>
+                <div class="messages" v-chat-scroll="{always: false, smooth: true}">
+                    <div v-for="message in messages" :key="message.id">
+                        <span class="text-info">[{{ message.name }}]: </span>
+                        <span>{{message.message}}</span>
                         <span class="text-secondary time">{{message.timestamp}}</span>
                     </div>
                 </div>
@@ -24,7 +26,6 @@
 import CreateMessage from '@/components/CreateMessage';
 import fb from '@/firebase/init';
 import moment from 'moment';
-
 export default {
     name: 'Chat',
     props: ['name'],
@@ -40,7 +41,7 @@ export default {
         let ref = fb.collection('messages').orderBy('timestamp');
         ref.onSnapshot(snapshot => {
             snapshot.docChanges().forEach(change => {
-                if(change.type = 'added') {
+                if (change.type = 'added') {
                     let doc = change.doc;
                     this.messages.push({
                         id: doc.id,
@@ -76,5 +77,3 @@ export default {
     overflow: auto;
 }
 </style>
-
-
