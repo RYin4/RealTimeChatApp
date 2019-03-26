33b0e40096d35a2accb2186ebd13495b91fdf7dc
Randy Yin <randyyin28@gmail.com>
2019-03-25 17:02:09 -0700

    Update CreateMessage.vue

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -2,8 +2,8 @@
     <div class="container" style="margin-bottom: 30px">
         <form @submit.prevent="createMessage">
             <div class="form-group">
-                <input type="text" name="message" class="form-control" placeholder="Enter Message..." v-model="newMessage">
-                <p class="text-danger" v-if="errorText">{{errorText}}</p>
+                <input type="text" name="message" class="form-control" placeholder="Enter message ..." v-model="newMessage">
+                <p class="text-danger" v-if="errorText">{{ errorText }}</p>
             </div>
             <button class="btn btn-primary" type="submit" name="action">Submit</button>
         </form>
@@ -12,7 +12,6 @@
 
 <script>
 import fb from '@/firebase/init';
-
 export default {
     name: 'CreateMessage',
     props: ['name'],
@@ -23,24 +22,21 @@ export default {
         }
     },
     methods: {
-        createMessage() {
+        createMessage () {
             if (this.newMessage) {
                 fb.collection('messages').add({
                     message: this.newMessage,
                     name: this.name,
-                    timestamp: Data.now()
+                    timestamp: Date.now()
                 }).catch(err => {
-                    console.log(err)
+                    console.log(err);
                 });
                 this.newMessage = null;
                 this.errorText = null;
-
             } else {
-                this.errorText = "A message must be entered first";
+                this.errorText = "A message must be entered first!";
             }
         }
-
-    }
+    }    
 }
 </script>
-
